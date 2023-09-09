965301a97f80aa7f4aaf7f37debd0359292e7eb3
Donald Alvarez <73081566+Donald-Alvarez@users.noreply.github.com>
2023-09-09 16:40:38 -0600

    Vista Control de la medicación terminada

---
+++ src/Utils/DietData.js
@@ -0,0 +1,74 @@
+export const DietData = [
+    {
+      id: 1,
+      title: 'Desayuno',
+      description: 'Platillos saludables para el desayuno.',
+      items: [
+        {
+          name: 'Tortilla de vegetales',
+          preparation:
+            'Ingredientes:\n- 2 huevos\n- 1/4 de pimiento rojo, cortado en cubos\n- 1/4 de pimiento verde, cortado en cubos\n- 1/4 de cebolla roja, cortada en cubos\n- Espinacas frescas\n- Aceite de oliva virgen extra\n- Sal y pimienta al gusto',
+          image: 'https://i.imgur.com/PVai24j.jpg',
+        },
+        {
+          name: 'Yogur con frutas',
+          preparation: 'Preparación detallada del Yogur con frutas.',
+          image: 'https://i.imgur.com/JSh4nPF.png',
+        },
+        {
+          name: 'Batido de proteínas',
+          preparation: 'Preparación detallada del Batido de proteínas.',
+          image: 'https://i.imgur.com/JSh4nPF.png',
+        },
+        {
+          name: 'Omelette de espinacas',
+          preparation: 'Preparación detallada del Omelette de espinacas.',
+          image: 'https://i.imgur.com/JSh4nPF.png',
+        },
+      ],
+    },
+    {
+      id: 2,
+      title: 'Almuerzo',
+      description: 'Platillos saludables para el almuerzo.',
+      items: [
+        {
+          name: 'Platillo 1 del almuerzo',
+          preparation: 'Preparación detallada del Platillo 1 del almuerzo.',
+          image: 'https://i.imgur.com/JSh4nPF.png',
+        },
+        {
+          name: 'Platillo 2 del almuerzo',
+          preparation: 'Preparación detallada del Platillo 2 del almuerzo.',
+          image: 'https://i.imgur.com/JSh4nPF.png',
+        },
+        {
+          name: 'Platillo 3 del almuerzo',
+          preparation: 'Preparación detallada del Platillo 3 del almuerzo.',
+          image: 'https://i.imgur.com/JSh4nPF.png',
+        },
+      ],
+    },
+    {
+      id: 3,
+      title: 'Cena',
+      description: 'Platillos saludables para la cena.',
+      items: [
+        {
+          name: 'Platillo 1 de la cena',
+          preparation: 'Preparación detallada del Platillo 1 de la cena.',
+          image: 'https://i.imgur.com/JSh4nPF.png',
+        },
+        {
+          name: 'Platillo 2 de la cena',
+          preparation: 'Preparación detallada del Platillo 2 de la cena.',
+          image: 'https://i.imgur.com/JSh4nPF.png',
+        },
+        {
+          name: 'Platillo 3 de la cena',
+          preparation: 'Preparación detallada del Platillo 3 de la cena.',
+          image: 'https://i.imgur.com/JSh4nPF.png',
+        },
+      ],
+    },
+  ]
