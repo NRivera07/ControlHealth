684a0b12ca1175b499df47423e5563c82eadf6ae
jonatan <villegasjoncena7@gmail.com>
2023-09-14 22:50:08 -0600

    actualizando rutas

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Quotes from './Views/Quotes/Quotes'
 import Diet from './Views/Diet/Diet'
 import Medication from './Views/Medication/Medication'
 import Routines from './Views/Routines/Routines'
+import Login from './Views/Login/Login'
+import Register from './Views/Register/Register'
 
 function App() {
   return (
@@ -14,6 +16,8 @@ function App() {
         <Navbar />
         <Switch>
           <Route path="/" exact component={Home} />
+          <Route path="/login" component={Login} />
+          <Route path="/sign-in" component={Register} />
           <Route path="/citas" component={Quotes} />
           <Route path="/medication" component={Medication} />
           <Route path="/diet" component={Diet} />
