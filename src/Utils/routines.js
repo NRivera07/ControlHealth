942695778a9051f6fe4d0c811fe85efc0f9186a1
Donald Alvarez <73081566+Donald-Alvarez@users.noreply.github.com>
2023-09-17 13:36:58 -0600

    Vista de rutinas culminada
    -Extensión React Player Instalada
    -Creación de estilos culminada.
    -Visualización del video con éxito.

---
+++ src/Utils/routines.js
@@ -0,0 +1,44 @@
+export const ExerciseData = [
+    {
+      id: 1,
+      title: 'Principiante',
+      description: 'Rutinas de ejercicio para fortalecer tus piernas.',
+      items: [
+        {
+          name: 'Full-Body',
+          description:
+            'Las sentadillas son un excelente ejercicio para fortalecer tus piernas y glúteos. Asegúrate de mantener una buena forma.',
+          image: 'https://i.imgur.com/0ZNovUx.png',
+          videoUrl: 'https://www.youtube.com/watch?v=lKc39ogHlDc',
+        },
+      ],
+    },
+    {
+      id: 2,
+      title: 'Intermedio',
+      description: 'Rutinas de ejercicio para tonificar tus brazos y hombros.',
+      items: [
+        {
+          name: 'Full-Body',
+          description:
+            'Las flexiones de brazos son ideales para fortalecer los músculos del pecho y los tríceps.',
+          image: 'https://i.imgur.com/0ZNovUx.png',
+          videoUrl: 'https://www.youtube.com/watch?v=lKc39ogHlDc',
+        },
+      ],
+    },
+    {
+      id: 3,
+      title: 'Avanzado',
+      description: 'Rutinas de ejercicio para fortalecer la espalda y mejorar la postura.',
+      items: [
+        {
+          name: 'Full-Body',
+          description:
+            'Las pull-ups son ideales para trabajar los músculos de la espalda y los brazos.',
+          image: 'https://i.imgur.com/0ZNovUx.png',
+          videoUrl: 'https://www.youtube.com/watch?v=lKc39ogHlDc',
+        },
+      ],
+    },
+  ];
